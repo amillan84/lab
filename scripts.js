061d2b3b6a09c01372d169fe303811d3b069cb4e
Aida Millan <amillan84@gmail.com>
2021-02-05 17:00:34 -0800

    changes to a couple files

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
-/*check and see if they entered the correct number */
-// Declare the variable and collect a number from the user
-<script>
-        let num = prompt('Guess a number between 1 and 10:');
-        if (num ==5) {
-            alert('You guessed the right number:')
-        } else {
-            alert('You guessed the wrong number:')
-        }
-    </script>
+/*
+App Name: First JavaScript Application
+Author: Aida Millan
+Date: 02/05/21
+*/
+
+// Allow the user to enter their guess
+let num = prompt('Guess a number between 1 and 10');
+// Check to see if the number is a winner
+if (num == 5) {
+    // If it is, they got it right
+    alert('You guessed the right number!');
+} else {
+    // If it isn't, they got it wrong
+    alert('You guessed the wrong number!');
+}
+
+
+
