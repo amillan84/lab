c022d77f392341483f16557279dc32593e23a663
Aida Millan <amillan84@gmail.com>
2021-02-05 16:28:47 -0800

    init commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*check and see if they entered the correct number */
+// Declare the variable and collect a number from the user
+<script>
+        let num = prompt('Guess a number between 1 and 10:');
+        if (num ==5) {
+            alert('You guessed the right number:')
+        } else {
+            alert('You guessed the wrong number:')
+        }
+    </script>
