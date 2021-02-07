514b3de9597e9cc5f97799487d26fde5ef1aeb4c
Aida Millan <amillan84@gmail.com>
2021-02-06 17:34:58 -0800

    added text in winners pop up

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ let num = prompt('Guess a number between 1 and 10');
 // Check to see if the number is a winner
 if (num == 5) {
     // If it is, they got it right
-    alert('You guessed the right number!');
+    alert('Congratulations!You guessed the right number!');
 } else {
     // If it isn't, they got it wrong
     alert('You guessed the wrong number!');
